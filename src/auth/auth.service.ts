4e33de51494a9016f71e96aad25fa562c148e7e3
DIO <jeremyzelaya@hotmail.es>
2021-03-10 10:24:05 -0600

    Added first signin flow

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { User } from './User.entity';
@@ -11,5 +11,14 @@ export class AuthService {
   ) {}
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.signUp(authCredentialsDto);
+  }
+
+  async signIn(AuthCredentialsDto: AuthCredentialsDto) {
+    const username = await this.userRepository.validateUserPassword(AuthCredentialsDto);
+    if (!username) {
+        throw new UnauthorizedException('Invalid credentials');
+    }
+    console.log(username);
+    
   }
 }
