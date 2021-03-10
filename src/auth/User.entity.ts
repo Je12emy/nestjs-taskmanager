e23fa0fcbf2f096cf87e766178432d922b3a2391
DIO <jeremyzelaya@hotmail.es>
2021-03-10 14:20:19 -0600

    Created relationship between entities

--- src/auth/User.entity.ts
+++ src/auth/User.entity.ts
@@ -2,10 +2,12 @@ import {
   BaseEntity,
   Column,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
   Unique,
 } from 'typeorm';
 import * as bcrypt from 'bcrypt';
+import { Task } from 'src/tasks/task.entity';
 
 @Entity()
 @Unique(['username'])
@@ -18,6 +20,10 @@ export class User extends BaseEntity {
   password: string;
   @Column()
   salt: string;
+
+
+  @OneToMany(type => Task, task => task.user, {eager: true}) // eager allows us to access the property either instatly or not, only one can be true
+  tasks: Task[];
 
   async validatePassword(password: string): Promise<boolean> {
     const hash = await bcrypt.hash(password, this.salt); // If the provided password after being hashed matches the password
--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,18 +1,27 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
-import { TaskStatus } from "./task-status.enum";
-
+import { User } from 'src/auth/User.entity';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { TaskStatus } from './task-status.enum';
 
 @Entity()
-export class Task extends BaseEntity{
-    @PrimaryGeneratedColumn() // This is a primary auto_incremt column
-    id: number;
+export class Task extends BaseEntity {
+  @PrimaryGeneratedColumn() // This is a primary auto_incremt column
+  id: number;
 
-    @Column()
-    title: string;
+  @Column()
+  title: string;
 
-    @Column()
-    description: string;
+  @Column()
+  description: string;
 
-    @Column()
-    status: TaskStatus
+  @Column()
+  status: TaskStatus;
+
+  @ManyToOne((type) => User, (user) => user.tasks, { eager: false })
+  user: User;
 }
