45753ebd52341427c94aa14adabb15c304d40a9f
DIO <jeremyzelaya@hotmail.es>
2021-03-10 09:23:32 -0600

    Added signup operations

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { User } from './User.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository) private userRepository: UserRepository,
   ) {}
+  async signUp(authCredentialsDto: AuthCredentialsDto) {
+    return this.userRepository.signUp(authCredentialsDto);
+  }
 }
