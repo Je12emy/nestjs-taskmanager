14f17bb88c688dd1f471679a7aeedaf37788587e
DIO <jeremyzelaya@hotmail.es>
2021-03-11 10:11:23 -0600

    Added cors set up

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,11 @@ async function bootstrap() {
 
   const logger = new Logger('bootstrap'); // Context for the logg message
   const app = await NestFactory.create(AppModule);
-  
+
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors(); // Accept request from the public
+  }
+
   const port = process.env.PORT || serverConfig.port; // PORT=3005 npm run start:dev
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
